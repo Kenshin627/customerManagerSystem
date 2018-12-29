92db71211bed62409444445c3d991e6c70bd75c6
kenshin627 <87zhaoxiaobo@163.com>
2018-12-29 19:27:03 +0800

    默认用户名密码

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -58,8 +58,8 @@ export default {
     }
     return {
       loginForm: {
-        username: 'admin',
-        password: 'admin'
+        username: 'administrator',
+        password: '1234567'
       },
       loginRules: {
         username: [{ required: true, trigger: 'blur', validator: validateUsername }],
